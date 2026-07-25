80f6e50f7e80cbc69995f8378ed819d6f776b8e3
Saverio Negro <saverionegro@gmail.com>
2026-07-26 00:46:26 +0200

    Initial Commit

---
+++ Cryptic/CrypticApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypticApp.swift
+//  Cryptic
+//
+//  Created by Saverio Negro on 7/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CrypticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
