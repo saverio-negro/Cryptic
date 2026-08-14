57c32e99596365bdbb16405b520156c5bbd3d367
Saverio Negro <saverionegro@gmail.com>
2026-08-14 02:07:14 +0200

    [Feature] Create `CoinImageViewModel` to manage the business logic that handles getting the coin image data to ultimately handle it to each `CoinImageView`

--- Cryptic/Core/Components/CoinImage/ViewModel/CoinImageViewModel.swift
+++ Cryptic/Core/Components/CoinImage/ViewModel/CoinImageViewModel.swift
@@ -5,16 +5,30 @@
 //  Created by Saverio Negro on 8/14/26.
 //
 
+import SwiftUI
+@preconcurrency import Combine
 
+@MainActor
 class CoinImageViewModel: ObservableObject {
     
     @Published var image: UIImage? = nil
     @Published var isLoading: Bool = false
-    let dataService: any CombineCoinImageDataService
+    private let dataService: any CombineCoinImageDataService
+    private var cancellables = Set<AnyCancellable>()
     
     init(dataService: any CombineCoinImageDataService) {
+        self.dataService = dataService
         Task {
-            
+            await self.dataService.publisher.sink(
+                receiveCompletion: { [weak self] _ in
+                    self?.isLoading = false
+                },
+                receiveValue: { [weak self] emittedImage in
+                    self?.image = emittedImage
+                }
+            )
+            .store(in: &cancellables)
         }
+        self.isLoading = true
     }
 }
